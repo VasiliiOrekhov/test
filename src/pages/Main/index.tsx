2af02ddcd581513a717d7705679fff9cb776b5fb
Vasilii Orekhov <vasilii.orekhov95@gmail.com>
2024-04-02 21:27:05 +0300

    added style to route

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -2,20 +2,28 @@ import { Menu } from 'antd';
 import { FC } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import s from './main.module.scss';
+import png from './../../assets/592238.png';
 
 export const MainPage: FC = () => {
   const navigate = useNavigate();
   return (
     <div className={s.wrapper}>
-      <Menu
-        onClick={({ key }) => {
-          navigate(key);
-        }}
-        defaultSelectedKeys={[window.location.pathname]}
-        items={[
-          { label: 'Children1', key: '/main/children1' },
-          { label: 'Children2', key: '/main/children2' },
-        ]}></Menu>
+      <div className={s.header}>
+        <img src={png} className={s.png} alt="Логотип" />
+        <Menu
+          className={s.menu}
+          onClick={({ key }) => {
+            navigate(key);
+          }}
+          mode="horizontal"
+          // selectedKeys={[selectedKey ? selectedKey : "/dashboard"]}
+          defaultSelectedKeys={[window.location.pathname]}
+          items={[
+            { label: 'Children1', key: '/main/children1' },
+            { label: 'Children2', key: '/main/children2' },
+          ]}></Menu>
+      </div>
+
       <Content />
     </div>
   );
